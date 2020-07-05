d30a3cd53adae2f03a21bbc744b89f396d5bff7c
Oscar <ozkar.ca@gmail.com>
2020-07-05 00:09:49 -0500

    ADD: Devise

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+	
+	# GET /articles
+	def index
+		@articles = Article.all
+	end
+
+	#GET /articles/:id
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	#GET /articles/new
+	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	#POST /articles
+	def create
+		@article = Article.new(article_params)
+		if @article.save 
+			redirect_to @article
+		else
+			render :new
+		end
+	end
+
+	#PUT /articles/:id
+	def update
+		@article = Article.find(params[:id]) 
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit
+		end
+	end
+
+	#DELETE /articles/:id
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
+
+end
